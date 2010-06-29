b76d57ed0d0e3ab62dc1880b23d2d48ef7bf2c43
hadley <h.wickham@gmail.com>
2010-06-29 15:26:39 -0500

    Tweaks and documentation for replay

--- R/replay.r
+++ R/replay.r
@@ -1,8 +1,11 @@
 #' Replay a list of evaluated results.
-#' Replay a list of evaluated results, just like you'd run them in a R
+#' Replay a list of evaluated results, as if you'd run them in an R
 #' terminal.
 #'
 #' @param x result from \code{\link{evaluate}}
+#' @aliases replay replay.list replay.character replay.source replay.warning
+#'   replay.message replay.error replay.value replay.recordedplot
+#' @export
 #' @examples
 #' samples <- system.file("tests", package = "eval.with.details")
 #' replay(evaluate(file(file.path(samples, "test-1.r"))))
@@ -10,18 +13,26 @@
 #' replay(evaluate(file(file.path(samples, "eval-output.r"))))
 replay <- function(x) UseMethod("replay", x)
 
-replay.list <- function(x) invisible(lapply(x, replay))
+replay.list <- function(x) {
+  invisible(lapply(x, replay))
+}
 
 replay.character <- function(x) {
   cat(x)
 }
-replay.source <- function(x) cat(line_prompt(x$src))
+
+replay.source <- function(x) {
+  cat(line_prompt(x$src))
+}
+
 replay.warning <- function(x) {
   message("Warning message:\n", x$message)
 }
+
 replay.message <- function(x) {
   message(gsub("\n$", "", x$message))
 }
+
 replay.error <- function(x) {
   if (is.null(x$call)) {
     message("Error: ", x$message)    
@@ -30,12 +41,26 @@ replay.error <- function(x) {
     message("Error in ", call, ": ", x$message)    
   }
 }
-replay.default <- function(x) {}
 
 replay.value <- function(x) {
   if (x$visible) print(x$value)
 }
 
+replay.value <- function(x) {
+  if (x$visible) print(x$value)
+}
+
+reply.recordedplot <- function(x) {
+  print(x)
+}
+
+#' Line prompt.
+#' Format a single expression as if it had been entered at the command prompt.
+#'
+#' @param x string representing a single expression
+#' @param prompt prompt for first line
+#' @param continue prompt for subsequent lines
+#' @return a string
 line_prompt <- function(x, prompt = options("prompt"), continue = options("continue"), ...) {
   lines <- strsplit(x, "\n")[[1]]
   n <- length(lines)
@@ -46,4 +71,3 @@ line_prompt <- function(x, prompt = options("prompt"), continue = options("conti
   
   paste(lines, "\n", collapse="")
 }
-
