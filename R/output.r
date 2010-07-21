a927cdda0217e8f380227bcd1cc2d7c7adaa003a
hadley <h.wickham@gmail.com>
2010-07-21 14:25:15 -0500

    Add testor for recordedplots

--- R/output.r
+++ R/output.r
@@ -6,14 +6,16 @@
 #'   is.source(x)
 #'   is.value(x)
 #'   is.warning(x)
-#' @export is.message is.warning is.error is.value is.source
-#' @aliases is.message is.warning is.error is.value is.source
+#'   is.recordedplot(x)
+#' @export is.message is.warning is.error is.value is.source is.recordedplot
+#' @aliases is.message is.warning is.error is.value is.source is.recordedplot
 #' @keywords internal
 is.message <- function(x) inherits(x, "message")
 is.warning <- function(x) inherits(x, "warning")
 is.error <- function(x) inherits(x, "error")
 is.value <- function(x) inherits(x, "value")
 is.source <- function(x) inherits(x, "source")
+is.recordedplot <- function(x) inherits(x, "recordedplot")
 
 new_value <- function(value, visible = TRUE) {
   structure(list(value = value, visible = visible), class = "value")
