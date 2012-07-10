402ad15d6fee3eb78fe976b2b2ecef22805eee9a
Yihui Xie <xie@yihui.name>
2012-07-09 23:22:26 -0400

    a test case for #15; not sure if it really works under Windows

--- inst/tests/test-parse.r
+++ inst/tests/test-parse.r
@@ -12,3 +12,10 @@ test_that("{ not removed", {
   expect_that(nrow(parse_all(f)), equals(1))
   
 })
+
+test_that("double quotes in Chinese characters not destroyed", {
+  loc <- Sys.getlocale("LC_ALL")
+  if (.Platform$OS.type == "windows" && grepl("Chinese (Simplified)_People's Republic of China.936", loc, fixed = TRUE)) {
+    expect_identical(parse_all(c('1+1', '"你好"'))[2, 1], '"你好"')
+  }
+})
