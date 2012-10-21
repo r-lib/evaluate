eedda3d2f15ff509b0a2ce06a497a7cefe973c4d
Michael Lawrence <michafla@gene.com>
2012-10-20 23:26:07 -0700

    New attempt: pass a set of output handler functions to evaluate(), so that the values can be interpreted in the temporally correct context (which matters for stateful things like R graphics). This takes the first attempt of the render argument one step further, where all outputs can be handled in a streaming style reminiscent of SAX.
    Processing the return value has always worked in the past, since
    values, once printed, are independent of state. Now that values do not
    need to always be printed (but often will be as part of a broader
    operation), we need to be careful about context.

--- R/eval.r
+++ R/eval.r
@@ -23,9 +23,13 @@
 #' @param new_device if \code{TRUE}, will open a new graphics device and
 #'   automatically close it after completion. This prevents evaluation from
 #'   interfering with your existing graphics environment.
+#' @param output_handler an instance of \code{\link{output_handler}}
+#'   that processes the output from the evaluation. The default simply
+#'   prints the visible return values.
 #' @import stringr
 evaluate <- function(input, envir = parent.frame(), enclos = NULL, debug = FALSE,
-                     stop_on_error = 0L, new_device = TRUE) {
+                     stop_on_error = 0L, new_device = TRUE,
+                     output_handler = new_output_handler()) {
   parsed <- parse_all(input)
 
   stop_on_error <- as.integer(stop_on_error)
@@ -47,7 +51,8 @@ evaluate <- function(input, envir = parent.frame(), enclos = NULL, debug = FALSE
     out[[i]] <- evaluate_call(
       parsed$expr[[i]][[1]], parsed$src[[i]],
       envir = envir, enclos = enclos, debug = debug, last = i == length(out),
-      use_try = stop_on_error != 2L)
+      use_try = stop_on_error != 2L,
+      output_handler = output_handler)
 
     if (stop_on_error > 0L) {
       errs <- vapply(out[[i]], is.error, logical(1))
@@ -65,7 +70,8 @@ evaluate <- function(input, envir = parent.frame(), enclos = NULL, debug = FALSE
 
 evaluate_call <- function(call, src = NULL,
                           envir = parent.frame(), enclos = NULL,
-                          debug = FALSE, last = FALSE, use_try = FALSE) {
+                          debug = FALSE, last = FALSE, use_try = FALSE,
+                          output_handler = new_output_handler()) {
   if (debug) message(src)
 
   if (is.null(call)) {
@@ -76,27 +82,47 @@ evaluate_call <- function(call, src = NULL,
   # Capture output
   w <- watchout(debug)
   on.exit(w$close())
-  output <- list(new_source(src))
+  source <- new_source(src)
+  output_handler$source(source)
+  output <- list(source)
+
+  handle_output <- function(plot = FALSE, incomplete_plots = FALSE) {
+    out <- w$get_new(plot, incomplete_plots)
+    if (!is.null(out$text))
+      output_handler$text(out$text)
+    if (!is.null(out$graphics))
+      output_handler$graphics(out$graphics)
+    output <<- c(output, out)
+  }
 
   # Hooks to capture plot creation
   capture_plot <- function() {
-    output <<- c(output, w$get_new(TRUE))
+    handle_output(TRUE)
   }
   old_hooks <- set_hooks(list(
     before.plot.new = capture_plot,
     before.grid.newpage = capture_plot))
   on.exit(set_hooks(old_hooks, "replace"), add = TRUE)
 
+  handle_condition <- function(cond) {
+    handle_output()
+    output <<- c(output, list(cond))
+  }
+
   # Handlers for warnings, errors and messages
   wHandler <- function(wn) {
-    output <<- c(output, w$get_new(), list(wn))
+    handle_condition(wn)
+    output_handler$warning(wn)
     invokeRestart("muffleWarning")
   }
   eHandler <- function(e) {
-    output <<- c(output, w$get_new(), list(e))
+    handle_condition(e)
+    output_handler$error(e)
+    invokeRestart("muffleWarning")
   }
   mHandler <- function(m) {
-    output <<- c(output, w$get_new(), list(m))
+    handle_condition(m)
+    output_handler$message(m)
     invokeRestart("muffleMessage")
   }
 
@@ -110,20 +136,25 @@ evaluate_call <- function(call, src = NULL,
   handle(ev <- withCallingHandlers(
     withVisible(eval(call, envir, enclos)),
     warning = wHandler, error = eHandler, message = mHandler))
-  output <- c(output, w$get_new(TRUE))
+  handle_output(TRUE)
 
-  # If visible, print and capture output
+  # If visible, process and capture output
   if (ev$visible) {
-    render <- if (isS4(ev$value)) show else print
-
-    try(withCallingHandlers(render(ev$value), warning = wHandler,
-      error = eHandler, message = mHandler), silent = TRUE)
-    output <- c(output, w$get_new(TRUE))
+    pv <- try(withCallingHandlers(withVisible(output_handler$value(ev$value)),
+      warning = wHandler, error = eHandler, message = mHandler), silent = TRUE)
+    handle_output(TRUE)
+    # If return value visible, print and capture output
+    if (pv$visible) {
+      try(withCallingHandlers(print(pv$value),
+            warning = wHandler, error = eHandler, message = mHandler),
+          silent = TRUE)
+      handle_output(TRUE)
+    }
   }
 
   # Always capture last plot, even if incomplete
   if (last) {
-    output <- c(output, w$get_new(TRUE, TRUE))
+    handle_output(TRUE, TRUE)
   }
 
   output
