51b58f1006da94e11aed0900b5f0e348b1b00bd2
hadley <h.wickham@gmail.com>
2012-11-28 13:07:37 -0600

    Disable broken test

--- inst/tests/test-errors.r
+++ inst/tests/test-errors.r
@@ -17,7 +17,12 @@ test_that("code errors if stop_on_error == 2L", {
 test_that("traceback useful if stop_on_error == 2L", {
   expect_error(evaluate(file("error-complex.r"), stop_on_error = 2L), "h()")
 
-  expect_match(.Traceback[[2]], "h()")
-  expect_match(.Traceback[[3]], "g()")
-  expect_match(.Traceback[[4]], "f()")
+  ## Doesn't work because .Traceback not create when code run
+  ## inside try or tryCatch. Can't figure out how to work around.
+  ## tryCatch(..., error = function(e) {}) doesn't have enough info
+  ## in e, or in the call stack.  options(error = function() {}) doesn't
+  ## stop error propagation
+  # expect_match(.Traceback[[2]], "h()")
+  # expect_match(.Traceback[[3]], "g()")
+  # expect_match(.Traceback[[4]], "f()")
 })
