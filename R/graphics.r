3cde23c5a904a2f4d3e41eddce512e6fad742b7c
Yihui Xie <xie@yihui.name>
2013-06-27 14:45:14 -0700

    should be single quote here

--- R/graphics.r
+++ R/graphics.r
@@ -1,6 +1,6 @@
 #" Capture snapshot of current device.
 #"
-#" There"s currently no way to capture when a graphics device changes,
+#" There's currently no way to capture when a graphics device changes,
 #" except to check its contents after the evaluation of every expression.
 #" This means that only the last plot of a series will be captured.
 #"
