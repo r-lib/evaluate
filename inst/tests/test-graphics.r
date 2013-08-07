cde485f66f24d781df4a79ff4995ebc4fa8bf33e
Yihui Xie <xie@yihui.name>
2013-08-06 18:03:27 -0700

    add two tests for layout() (#25)

--- inst/tests/test-graphics.r
+++ inst/tests/test-graphics.r
@@ -71,6 +71,18 @@ test_that("multirow graphics are captured on close", {
               equals(c(rep("source", 4), "recordedplot")))
 })
 
+test_that("plots are captured in a non-rectangular layout", {
+  ev <- evaluate(file("plot-multi-layout.r"))
+
+  expect_that(classes(ev),
+              equals(rep(c("source", "recordedplot"), c(1, 3))))
+
+  ev <- evaluate(file("plot-multi-layout2.r"))
+
+  expect_that(classes(ev),
+              equals(rep(c("source", "recordedplot"), c(4, 2))))
+})
+
 test_that("changes in parameters don't generate new plots", {
   ev <- evaluate(file("plot-par.r"))
   expect_that(classes(ev),
