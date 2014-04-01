6ca89cc10473c678a83c36b627b9e46087c3fb5d
hadley <h.wickham@gmail.com>
2014-04-01 10:56:29 -0700

    Add test for graphics output handler

--- tests/testthat/test-output-handler.R
+++ tests/testthat/test-output-handler.R
@@ -7,3 +7,11 @@ test_that("text output handler is called with text", {
   evaluate("print('abc')", output_handler = oh)
   expect_equal(text, "[1] \"abc\"\n")
 })
+
+test_that("graphic output handler not called with no graphics", {
+  graphics <- NULL
+  oh <- new_output_handler(graphics = function(o) graphics <<- 1)
+
+  evaluate("print('abc')", output_handler = oh)
+  expect_equal(graphics, NULL)
+})
