1d3b88d35c8d46a33b7a252552b4ca15f90bb23c
Hadley Wickham <h.wickham@gmail.com>
2024-06-17 12:43:35 +0100

    Centralise value handling in one place
    One small step to moving more logic into the output handler and out of `evaluate()`.

--- R/eval.R
+++ R/eval.R
@@ -231,7 +231,6 @@ evaluate_call <- function(call,
   } else {
     handle <- force
   }
-  value_handler <- output_handler$value
   if (include_timing) {
     timing_fn <- function(x) system.time(x)[1:3]
   } else {
@@ -249,7 +248,6 @@ evaluate_call <- function(call,
 
   user_handlers <- output_handler$calling_handlers
 
-  multi_args <- length(formals(value_handler)) > 1
   for (expr in call) {
     srcindex <- length(output)
     time <- timing_fn(handle(
@@ -264,14 +262,10 @@ evaluate_call <- function(call,
     if (!is.null(time))
       attr(output[[srcindex]]$src, 'timing') <- time
 
-    # If visible or the value handler has multi args, process and capture output
-    if (ev$visible || multi_args) {
+    if (show_value(output_handler, ev$visible)) {
       pv <- list(value = NULL, visible = FALSE)
-      value_fun <- if (multi_args) value_handler else {
-        function(x, visible) value_handler(x)
-      }
       handle(pv <- withCallingHandlers(withVisible(
-        value_fun(ev$value, ev$visible)
+        handle_value(output_handler, ev$value, ev$visible)
       ), warning = wHandler, error = eHandler, message = mHandler))
       handle_output(TRUE)
       # If the return value is visible, save the value to the output
