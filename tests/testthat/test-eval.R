be1f9cfc402f4ac8ef2495519d267364fd220b6a
Hadley Wickham <h.wickham@gmail.com>
2024-06-21 09:40:15 +0100

    Move evaluation class into own file (#165)
    And use `output_type()` in print method

--- tests/testthat/test-eval.R
+++ tests/testthat/test-eval.R
@@ -84,18 +84,3 @@ test_that("multiple lines of comments do not lose the terminating \\n", {
   expect_output_types(ev, c("source", "source"))
   expect_equal(ev[[1]]$src, "# foo\n")
 })
-
-
-test_that("has a reasonable print method", {
-  f <- function() {
-    print("1")
-    message("2")
-    warning("3")
-    stop("4")
-  }
-
-  expect_snapshot({
-    evaluate("f()")
-    evaluate("plot(1:3)")
-  })  
-})
