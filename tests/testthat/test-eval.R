dd4e7a2504535a03bc756ee13ac5f2b4005fc6dd
Hadley Wickham <h.wickham@gmail.com>
2024-06-21 08:42:56 +0100

    Move test-evalute.R tests to test-eval.R

--- tests/testthat/test-eval.R
+++ tests/testthat/test-eval.R
@@ -1,4 +1,12 @@
 
+test_that("file with only comments runs", {
+  ev <- evaluate_("
+    # This test case contains no executable code
+    # but it shouldn't throw an error
+  ")
+  expect_output_types(ev, c("source", "source"))
+})
+
 test_that("can evaluate expressions of all lengths", {
   source <- "
     # a comment
@@ -22,6 +30,71 @@ test_that("log_echo causes output to be immediately written to stderr()", {
   expect_output_types(res, c("source", "text"))
   expect_equal(res[[1]]$src, "f()")
 })
+
+test_that("data sets loaded", {
+  skip_if_not_installed("lattice")
+
+  ev <- evaluate_('
+    data(barley, package = "lattice")
+    barley
+  ')
+  expect_output_types(ev, c("source", "source", "text"))
+})
+
+test_that("terminal newline not needed", {
+  ev <- evaluate("cat('foo')")
+  expect_output_types(ev, c("source", "text"))
+  expect_equal(ev[[2]], "foo")
+})
+
+test_that("S4 methods are displayed with show, not print", {
+  methods::setClass("A", contains = "function", where = environment())
+  methods::setMethod("show", "A", function(object) cat("B"))
+  a <- methods::new('A', function() b)
+
+  ev <- evaluate("a")
+  expect_equal(ev[[2]], "B")
+})
+
+test_that("errors during printing visible values are captured", {
+  methods::setClass("A", contains = "function", where = environment())
+  methods::setMethod("show", "A", function(object) stop("B"))
+  a <- methods::new('A', function() b)
+
+  ev <- evaluate("a")
+  expect_output_types(ev, c("source", "error"))
+})
+
+
+test_that("output and plots interleaved correctly", {
+  ev <- evaluate_("
+    for (i in 1:2) {
+      cat(i)
+      plot(i)
+    }
+  ")
+  expect_output_types(ev, c("source", "text", "plot", "text", "plot"))
+
+  ev <- evaluate_("
+    for (i in 1:2) {
+      plot(i)
+      cat(i)
+    }
+  ")
+  expect_output_types(ev, c("source", "plot", "text", "plot", "text"))
+})
+
+test_that("multiple expressions on one line can get printed as expected", {
+  ev <- evaluate("x <- 1; y <- 2; x; y")
+  expect_output_types(ev, c("source", "text", "text"))
+})
+
+test_that("multiple lines of comments do not lose the terminating \\n", {
+  ev <- evaluate("# foo\n#bar")
+  expect_output_types(ev, c("source", "source"))
+  expect_equal(ev[[1]]$src, "# foo\n")
+})
+
 
 test_that("has a reasonable print method", {
   f <- function() {
