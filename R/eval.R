bfb7f4efa050e2cb5ada28d0aef5edddf28ac66f
Hadley Wickham <h.wickham@gmail.com>
2024-06-26 09:26:46 +0100

    Move handlers into their own file in conditions.R

--- R/eval.R
+++ R/eval.R
@@ -147,48 +147,14 @@ evaluate_top_level_expression <- function(exprs,
   local_plot_hooks(watcher$capture_plot_and_output)
 
   # Handlers for warnings, errors and messages
-  mHandler <- function(cnd) {
-    watcher$capture_plot_and_output()
-    
-    if (on_message$capture) {
-      watcher$push(cnd)
-      output_handler$message(cnd)
-    }
-    if (on_message$silence) {
-      invokeRestart("muffleMessage")
-    }
-  }
-  wHandler <- function(cnd) {
-    # do not handle warnings that shortly become errors
-    if (getOption("warn") >= 2) return()
-    # do not handle warnings that have been completely silenced
-    if (getOption("warn") < 0) return()
-
-    watcher$capture_plot_and_output()
-    if (on_warning$capture) {
-      cnd <- reset_call(cnd)
-      watcher$push(cnd)
-      output_handler$warning(cnd)
-    }
-    if (on_warning$silence) {
-      invokeRestart("muffleWarning")
-    }
-  }
-  eHandler <- function(cnd) {
-    watcher$capture_plot_and_output()
-    
-    cnd <- reset_call(cnd)
-    watcher$push(cnd)
-    
-    switch(on_error,
-      continue = invokeRestart("eval_continue"),
-      stop = invokeRestart("eval_stop"),
-      error = invokeRestart("eval_error", cnd)
-    )
-  }
-
   user_handlers <- output_handler$calling_handlers
-  evaluate_handlers <- list(error = eHandler, warning = wHandler, message = mHandler)
+  evaluate_handlers <- condition_handlers(
+    watcher,
+    output_handler,
+    on_error = on_error,
+    on_warning = on_warning,
+    on_message = on_message
+  )
   # The user's condition handlers have priority over ours
   handlers <- c(user_handlers, evaluate_handlers)
 
@@ -219,22 +185,6 @@ evaluate_top_level_expression <- function(exprs,
   }
 
   invisible()
-}
-
-with_handlers <- function(code, handlers) {
-  if (!is.list(handlers)) {
-    stop("`handlers` must be a list", call. = FALSE)
-  }
-
-  call <- as.call(c(quote(withCallingHandlers), quote(code), handlers))
-  eval(call)
-}
-
-reset_call <- function(cnd) {
-  if (identical(cnd$call, quote(eval(expr, envir)))) {
-    cnd$call <- NULL
-  }
-  cnd
 }
 
 check_stop_on_error <- function(x) {
