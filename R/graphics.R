32ebbc7afa08ff4844b305018f384a42636529f0
Hadley Wickham <h.wickham@gmail.com>
2024-07-11 07:25:25 +0200

    WIP: only show final plots
    Fixes #206

--- R/graphics.R
+++ R/graphics.R
@@ -9,6 +9,8 @@ local_plot_hooks <- function(f, frame = parent.frame()) {
   invisible()
 }
 
+# visual changes ---------------------------------------------------------
+
 looks_different <- function(old_dl, new_dl) {
   if (identical(old_dl, new_dl)) {
     return(FALSE)
@@ -54,3 +56,40 @@ non_visual_calls <- c(
   "C_strHeight", "C_strWidth", 
   "palette", "palette2"
 )
+
+
+# plot trimming ----------------------------------------------------------
+
+# Eliminate plots that are modified by subsequent lines so that we only
+# show the final plot
+trim_plots <- function(x) {
+  if (!is_evaluation(x)) {
+    stop("`x` must be an evaluation object.")
+  }
+
+  is_plot <- vapply(x, is.recordedplot, logical(1))
+  plot_idx <- which(is_plot)
+  keep <- rep(TRUE, length(plot_idx))
+
+  prev_plot <- NULL
+  for (i in seq2(2, length(plot_idx))) {
+    cur_plot_dl <- x[[plot_idx[i]]][[1]]
+    prev_plot_dl <- x[[plot_idx[i - 1]]][[1]]
+
+    if (prev_plot_dl %is_prefix_of% cur_plot_dl) {
+      keep[i - 1] <- FALSE
+    }
+  }
+
+  idx <- seq_along(x)
+  idx <- setdiff(idx, plot_idx[!keep])
+  x[idx]
+}
+
+`%is_prefix_of%` <- function(x, y) {
+  if (length(x) > length(y)) {
+    return(FALSE)
+  }
+
+  identical(x[], y[seq_along(x)])
+}
