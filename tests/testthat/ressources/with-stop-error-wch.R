c6ce2c963f6c08c2bc777fbadaf0c5857c0ab24b
Christophe Dervieux <christophe.dervieux@gmail.com>
2024-12-20 15:50:34 +0100

    Fix wrong error in test file
    because g() was missing

--- tests/testthat/ressources/with-stop-error-wch.R
+++ tests/testthat/ressources/with-stop-error-wch.R
@@ -3,6 +3,7 @@ withCallingHandlers(
   error = function(cnd) rlang::entrace(cnd),
   evaluate::evaluate(function() {
     f <- function() g()
+    g <- function() h()
     h <- function() stop("!")
     f()
   }, stop_on_error = 2L)
